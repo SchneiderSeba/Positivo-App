60662d7654d2a99eb2f1779bf54eae7c4b0deb3e
SchneiderSeba <seba_19_sc@hotmail.com>
2025-12-28 17:43:29 -0300

    ENV FIX

--- BackEnd/supa-client.js
+++ BackEnd/supa-client.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = process.env.PUBLIC_SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.PUBLIC_SUPABASE_ANON_KEY;
+const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
   throw new Error('Faltan las variables de entorno de Supabase. Por favor configura EXPO_PUBLIC_SUPABASE_URL y EXPO_PUBLIC_SUPABASE_ANON_KEY en .env.local')
